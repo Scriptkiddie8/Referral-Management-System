e861145982c9c407e4303f8f6a183dfc1ff9b6d6
kartik <work.kartikgarg@gmail.com>
2026-01-10 21:50:59 +0530

    Fix CORS for local frontend with deployed backend

--- server/src/app.js
+++ server/src/app.js
@@ -6,14 +6,18 @@ import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 const app = express();
 
-const allowedOrigins = [
-  // "https://referral-workoai.vercel.app",
-  "http://localhost:5000", // optional, for local testing
-];
-
 app.use(express.json());
-// app.use(cors({ origin: "https://referral-workoai.vercel.app/" }));
-app.use(cors());
+
+// app.use(cors());
+
+app.set("trust proxy", 1);
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/candidates", candidateRoutes);
 app.use("/uploads", express.static("uploads"));

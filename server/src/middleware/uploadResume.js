0ee6d8859172d64d51275cea7181f0e2f6f20d50
kartik <work.kartikgarg@gmail.com>
2026-01-10 23:03:01 +0530

    Cloudinary added for the data storage(resume)

--- server/src/middleware/uploadResume.js
+++ server/src/middleware/uploadResume.js
@@ -1,16 +1,19 @@
-import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
+import path from "path";
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "resumes",
-    resource_type: "raw", // IMPORTANT for PDFs
-    allowed_formats: ["pdf"],
+    resource_type: "raw", // raw is required for PDFs
+    allowed_formats: ["pdf"], // only allow PDFs
+    public_id: (req, file) => {
+      // preserve original name (without spaces) + timestamp
+      const name = file.originalname.split(".")[0].replace(/\s+/g, "_");
+      return `${name}_${Date.now()}`; // extension will be automatically added by Cloudinary
+    },
   },
 });
 
-const uploadResume = multer({ storage });
-
-export default uploadResume;
+export default storage;

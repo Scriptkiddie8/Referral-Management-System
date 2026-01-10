77e302a55c946fa24bc02baa2c55ec0613ce2291
kartik <work.kartikgarg@gmail.com>
2026-01-10 14:59:49 +0530

    Frontend ready for deployement

--- server/src/app.js
+++ server/src/app.js
@@ -7,24 +7,13 @@ import authRoutes from "./routes/auth.routes.js";
 const app = express();
 
 const allowedOrigins = [
-  "https://referral-workoai.vercel.app",
+  // "https://referral-workoai.vercel.app",
   "http://localhost:5000", // optional, for local testing
 ];
 
 app.use(express.json());
 // app.use(cors({ origin: "https://referral-workoai.vercel.app/" }));
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // allow tools like Postman
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `CORS blocked: ${origin}`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 
 app.use("/api/candidates", candidateRoutes);
 app.use("/uploads", express.static("uploads"));

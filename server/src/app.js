ffebe5778e520a8079d155ac868f838a292f65a5
kartik <work.kartikgarg@gmail.com>
2026-01-11 17:39:23 +0530

    Fix CORS for Frontend

--- server/src/app.js
+++ server/src/app.js
@@ -6,19 +6,18 @@ import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 const app = express();
 
-app.use(express.json());
-
-// app.use(cors());
-
-app.set("trust proxy", 1);
-
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: [
+      "http://localhost:5174",
+      "https://referral-management-system-1.onrender.com",
+    ],
     credentials: true,
   })
 );
 
+app.use(express.json());
+app.set("trust proxy", 1);
 app.use("/api/candidates", candidateRoutes);
 app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
